df8845676f386bab6f848ad21604c105a8342cf9
Juan Hamilton <hedwards507@gmail.com>
2021-04-02 21:44:57 -0500

    Added animations

--- app/src/main/java/com/example/simpletasks/ui/components/SelectLabelRow.kt
+++ app/src/main/java/com/example/simpletasks/ui/components/SelectLabelRow.kt
@@ -1,5 +1,6 @@
 package com.example.simpletasks.ui.components
 
+import androidx.compose.animation.core.animateFloatAsState
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.interaction.MutableInteractionSource
 import androidx.compose.foundation.layout.Row
@@ -10,12 +11,13 @@ import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.rounded.ExpandLess
 import androidx.compose.material.icons.rounded.ExpandMore
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
 import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.rotate
 import androidx.compose.ui.res.dimensionResource
 import androidx.compose.ui.res.stringResource
 import com.example.simpletasks.R
@@ -37,22 +39,20 @@ fun SelectLabelRow(isExpanded: Boolean, onExpandChange: () -> Unit) {
                     vertical = dimensionResource(id = R.dimen.space_between_10)
                 )
         ) {
+            val degrees by animateFloatAsState(if (isExpanded) 180f else 0f)
             Text(
                 text = stringResource(id = R.string.select_label_color),
                 style = MaterialTheme.typography.subtitle1
             )
             Spacer(modifier = Modifier.padding(dimensionResource(id = R.dimen.space_between_2)))
-            if (isExpanded) {
-                Icon(
-                    imageVector = Icons.Rounded.ExpandLess,
-                    contentDescription = stringResource(id = R.string.hide_labels)
-                )
-            } else {
-                Icon(
-                    imageVector = Icons.Rounded.ExpandMore,
-                    contentDescription = stringResource(id = R.string.show_labels)
-                )
-            }
+            Icon(
+                imageVector = Icons.Rounded.ExpandMore,
+                contentDescription = if (isExpanded)
+                    stringResource(id = R.string.hide_labels)
+                else
+                    stringResource(id = R.string.show_labels),
+                modifier = Modifier.rotate(degrees)
+            )
         }
     }
 }
